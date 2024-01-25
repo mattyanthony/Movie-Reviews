0d20509b2942ea43746efdcc0d2505863b963955
mattyanthony <108240399+mattyanthony@users.noreply.github.com>
2024-01-25 16:09:22 -0500

    Major style changes including hover feature, backround, general aesthetic

--- moviereviews/src/App.jsx
+++ moviereviews/src/App.jsx
@@ -7,52 +7,62 @@ const App = () => {
     {
       title: "Django Unchained",
       imageUrl: "https://resizing.flixster.com/6_9WVndz2aGcjCvQM_oyRo0aO9U=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2VkMjk3ZGNhLThhOTUtNGQ5YS1iNWYwLTczMWEzNDBiZjQwZC53ZWJw",
-      reviewUrl: "https://www.rottentomatoes.com/m/django_unchained_2012"
+      reviewUrl: "https://www.rottentomatoes.com/m/django_unchained_2012",
+      description: "Western/Drama"
     },
     {
       title: "Dirty Dancing",
       imageUrl: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/NowShowing/2027/2027_aa.jpg",
-      reviewUrl: "https://www.rottentomatoes.com/m/dirty_dancing"
+      reviewUrl: "https://www.rottentomatoes.com/m/dirty_dancing",
+      description: "Romance/Drama"
     },
     {
       title: "Interstellar",
       imageUrl: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10543523_p_v12_ar.jpg",
-      reviewUrl: "https://www.rottentomatoes.com/m/interstellar_2014"
+      reviewUrl: "https://www.rottentomatoes.com/m/interstellar_2014",
+      description: "Sci-fi/Adventure"
     },
     {
       title: "The Dark Knight",
       imageUrl: "https://resizing.flixster.com/Wg25mLoPWxjcxXzNyaSF4VGgGE4=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2ZiNjZiNWFkLWVhNzEtNDRhMC1iNGIwLTFmY2FkNzllNTJlMi5qcGc=",
-      reviewUrl: "https://www.rottentomatoes.com/m/the_dark_knight"
+      reviewUrl: "https://www.rottentomatoes.com/m/the_dark_knight",
+      description: "Action/Adventure"
     },
     {
       title: "Pulp Fiction",
       imageUrl: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p15684_p_v8_af.jpg",
-      reviewUrl: "https://www.rottentomatoes.com/m/pulp_fiction"
+      reviewUrl: "https://www.rottentomatoes.com/m/pulp_fiction",
+      description: "Drama"
     },
     {
       title: "Fight Club",
       imageUrl: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p23069_p_v8_aa.jpg",
-      reviewUrl: "https://www.rottentomatoes.com/m/fight_club"
+      reviewUrl: "https://www.rottentomatoes.com/m/fight_club",
+      description: "Thriller"
     },
     {
       title: "American Sniper",
       imageUrl: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10991238_p_v8_ay.jpg",
-      reviewUrl: "https://www.rottentomatoes.com/m/american_sniper"
+      reviewUrl: "https://www.rottentomatoes.com/m/american_sniper",
+      description: "War/Biography"
     },
     {
       title: "Oppenheimer",
       imageUrl: "https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_.jpg",
-      reviewUrl: "https://www.rottentomatoes.com/m/oppenheimer_2023"
+      reviewUrl: "https://www.rottentomatoes.com/m/oppenheimer_2023",
+      description: "Historical Drama"
     },
     {
       title: "Star Wars: Episode 3 - Revenge of the Sith",
       imageUrl: "https://resizing.flixster.com/-Mjmf-8IzcGhEpF81dEbL7xUoCM=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzg1MmZlZWZjLWVkYjgtNGM2Zi1iOGZhLWI1ZTdhYTVkZjgyYi53ZWJw",
-      reviewUrl: "https://www.rottentomatoes.com/m/star_wars_episode_iii_revenge_of_the_sith"
+      reviewUrl: "https://www.rottentomatoes.com/m/star_wars_episode_iii_revenge_of_the_sith",
+      description: "Sci-fi/Adventure"
     },
     {
       title: "Kingdom of Heaven",
       imageUrl: "https://m.media-amazon.com/images/I/71y3iJWWjHL._AC_UF894,1000_QL80_.jpg",
-      reviewUrl: "https://www.rottentomatoes.com/m/kingdom_of_heaven"
+      reviewUrl: "https://www.rottentomatoes.com/m/kingdom_of_heaven",
+      description: "Historical Drama"
     }
   ];
   
--- moviereviews/src/MovieCard.jsx
+++ moviereviews/src/MovieCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const MovieCard = ({ title, imageUrl, reviewUrl }) => {
+const MovieCard = ({ title, imageUrl, reviewUrl, description }) => {
   return (
     <div className="movie-card">
       <img src={imageUrl} alt={title} />
       <div className="container">
         <h4>{title}</h4>
-        <a href={reviewUrl} target="_blank" rel="noopener noreferrer">Rotten Tomatoes</a>
+        <p className="movie-genre">{description}</p> 
+        <a href={reviewUrl} target="_blank" rel="noopener noreferrer">Reviews</a>
       </div>
     </div>
   );
